114a02cb55304c409591024303179bc6842a7a63
ogawa <ogawa@r-up.jp>
2013-11-15 19:20:21 +0900

    タブバーをタップしても遷移させずに、モーダルを立ち上げるサンプル作った
    https://github.com/mixi-inc/iOSTraining/wiki/HomeWork-2.1-%E3%82%BF%E3%83%96%E3%83%90%E3%83%BC%E3%81%8B%E3%82%89%E3%83%A2%E3%83%BC%E3%83%80%E3%83%AB%E3%83%93%E3%83%A5%E3%83%BC%E3%82%92%E8%A1%A8%E7%A4%BA%E3%81%99%E3%82%8B

---
+++ mixi.iOSTraining/homework/ModalFromTab/MFTModalViewController.h
@@ -0,0 +1,20 @@
+//
+//  MFTModalViewController.h
+//  ModalFromTab
+//
+//  Created by 武田 祐一 on 2013/04/17.
+//  Copyright (c) 2013年 武田 祐一. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@protocol MTFModalViewControllerDelegate;
+
+@interface MFTModalViewController : UIViewController
+- (IBAction)closeButtonTapped:(id)sender;
+@property (nonatomic, weak) id<MTFModalViewControllerDelegate> delegate;
+@end
+
+
+@protocol MTFModalViewControllerDelegate <NSObject>
+- (void)modalViewControllerCloseButtonTapped:(MFTModalViewController *)viewController;
+@end
