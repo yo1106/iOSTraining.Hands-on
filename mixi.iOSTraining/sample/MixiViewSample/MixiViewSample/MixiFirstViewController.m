02ee2528cf4a8d1e8840a3fbf36dc772abcb1993
ogawa <ogawa@r-up.jp>
2013-11-18 20:19:40 +0900

    これはただの例文 https://github.com/mixi-inc/iOSTraining/wiki/3.1-UIView

---
+++ mixi.iOSTraining/sample/MixiViewSample/MixiViewSample/MixiFirstViewController.m
@@ -0,0 +1,58 @@
+//
+//  MixiFirstViewController.m
+//  MixiViewSample
+//
+//  Created by 田村 航弥 on 2013/04/18.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import "MixiFirstViewController.h"
+
+@interface MixiFirstViewController ()
+
+@end
+
+@implementation MixiFirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"First", @"First");
+        self.tabBarItem.image = [UIImage imageNamed:@"first"];
+    }
+    return self;
+}
+							
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"%@", self.view.subviews);
+    for (UIView *view in self.view.subviews){
+        if([view isKindOfClass:[UIButton class]]){
+            UIView *subview = [[UIView alloc] initWithFrame:view.frame];
+            [subview setBackgroundColor:[UIColor redColor]];
+            subview.alpha = 0.5;
+            [self.view insertSubview:subview belowSubview:view];
+        }
+
+        if([view isKindOfClass:[UILabel class]]){
+            NSLog(@"UILabel");
+            UIView *subview = [[UIView alloc] initWithFrame:view.frame];
+            [subview setBackgroundColor:[UIColor blueColor]];
+            subview.alpha = 0.5;
+            [self.view insertSubview:subview belowSubview:view];
+        }
+    }
+
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ mixi.iOSTraining/sample/MixiViewSample/MixiViewSample/MixiSecondViewController.m
@@ -0,0 +1,45 @@
+//
+//  MixiSecondViewController.m
+//  MixiViewSample
+//
+//  Created by 田村 航弥 on 2013/04/18.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import "MixiSecondViewController.h"
+
+@interface MixiSecondViewController ()
+- (IBAction)pressButton:(UIButton *)sender;
+@end
+
+@implementation MixiSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Second", @"Second");
+        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+    }
+    return self;
+}
+							
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+
+//    [[self.view viewWithTag:5] removeFromSuperview];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pressButton:(UIButton *)sender
+{
+    NSLog(@"%d", sender.tag);
+}
+@end
