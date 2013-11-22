405d1c7d748510950c57a73d8ba7b12628545319
ogawa <ogawa@r-up.jp>
2013-11-22 15:40:55 +0900

    イメージPickerのサンプルプロジェクト追加。 このままじゃアスペクト比がオカシイぜ！ https://github.com/mixi-inc/iOSTraining/wiki/5.1-UIImagePickerController

---
+++ mixi.iOSTraining/sample/MixiImagePickerSample/MixiImagePickerSample/MixiViewController.h
@@ -0,0 +1,15 @@
+//
+//  MixiViewController.h
+//  MixiImagePickerSample
+//
+//  Created by 田村 航弥 on 2013/04/22.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixiViewController : UIViewController
+<UIImagePickerControllerDelegate,
+UINavigationControllerDelegate>
+
+@end
---
+++ mixi.iOSTraining/sample/MixiImagePickerSample/MixiImagePickerSample/MixiViewController.m
@@ -0,0 +1,60 @@
+//
+//  MixiViewController.m
+//  MixiImagePickerSample
+//
+//  Created by 田村 航弥 on 2013/04/22.
+//  Copyright (c) 2013年 mixi. All rights reserved.
+//
+
+#import "MixiViewController.h"
+
+@interface MixiViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+- (IBAction)pressCameraButton:(id)sender;
+
+@end
+
+@implementation MixiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pressCameraButton:(id)sender
+{
+    UIImagePickerController *imagePickerVC = [[UIImagePickerController alloc] init];
+    // UIImagePickerControllerSourceTypeSavedPhotosAlbum だと直接写真選択画面
+    imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    // 選択したメディアの編集を可能にするかどうか
+    imagePickerVC.allowsEditing = YES;
+
+    // 選択可能なメディアの制限 デフォルトは photo のみ。
+    // movie を選択可能にするには
+    imagePickerVC.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:imagePickerVC.sourceType];
+    imagePickerVC.delegate = self;
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+#pragma mark - UIImagePickerDelegate methods
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
+    //Cropした位置情報取得だぜ
+    CGRect rect;
+    [[info objectForKey:UIImagePickerControllerCropRect] getValue:&rect];
+
+    [_photoImageView setImage:info[UIImagePickerControllerOriginalImage]];
+}
+
+
+@end
